3243600d482c45a73311bca7e70fd9b4da7c47af
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-07-17 16:11:15 +0100

    DONE: faceCascade sorted. TODO: Detect faces function. Ref: https://www.pythonforengineers.com/image-and-video-processing-in-python/; https://realpython.com/face-recognition-with-python/

--- face_detect.py
+++ face_detect.py
@@ -6,10 +6,17 @@ imagePath = sys.argv[1]
 cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
-face_cascade &lt; span class="pl-k"&gt; =&lt; /span&gt; cv2.CascadeClassifier(cascasdepath)
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+)
