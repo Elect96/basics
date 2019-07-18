a2a9511800f55164044143ddecea43f9b16755ce
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-07-18 19:27:58 +0100

    DONE: Face detection. TODO: Webcam face detection. Ref: https://www.pythonforengineers.com/image-and-video-processing-in-python/; https://realpython.com/face-recognition-with-python/

---
+++ webcam_face_detect.py
@@ -0,0 +1,9 @@
+import cv2
+import sys
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
