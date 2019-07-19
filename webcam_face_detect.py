a3891dbd60080be872ee51907a4cce3f886a711d
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-07-19 14:46:39 +0100

    DONE: Webcam face detection. TODO: Motion detection. Ref: https://www.pythonforengineers.com/image-and-video-processing-in-python/; https://realpython.com/face-recognition-with-python/

--- webcam_face_detect.py
+++ webcam_face_detect.py
@@ -1,9 +1,52 @@
 import cv2
 import sys
 
-# Get user supplied values
-imagePath = sys.argv[1]
 cascPath = "haarcascade_frontalface_default.xml"
-
-# Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+
+# VideoCapture(0) always points to the default webcam
+# if no video is specified
+# else: it loads the file
+if len(sys.argv) < 2:
+    video_capture = cv2.VideoCapture(0)
+else:
+    video_capture = cv2.VideoCapture(sys.argv[1])
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", frame)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
